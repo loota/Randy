b3b33039b2707c64f0ae349339a6588e6af648da
heikki <heikki@nassikka.pp.htv.fi>
2011-08-31 20:45:32 +0300

    Move the cli option handling to a class

--- cliRandy.php
+++ cliRandy.php
@@ -1,5 +1,4 @@
 <?php
-// @TODO Add Usage to cli
 require_once 'randy.php';
 
 class CommandLineExerciser extends Exerciser {
@@ -27,65 +26,81 @@ class CommandLineView implements View {
     }
 }
 
-function usage()
+class ExerDriver
 {
-    $usage = "Usage: php cliRandy.php [--no-log] [NUMBER OF EXERCISES]
-    Show <number> of exercises
-     php cliRandy.php <number>
-    
-    * start interactive mode
-     php cliRandy.php
-    
-    * In the interactive mode the following commands apply: 
-        q quits
-        enter key shows one exercise. 
-        Typing a number and then enter shows that number of exercises";
-    return $usage;
-}
+    private $numberOfExercises = 0;
+    private $logging = true;
 
-$exer = new CommandLineExerciser();
-$exer->addView(new CommandLineView());
+    private function _checkOptions($options)
+    {
+        if (is_array($options)) {
+            // remove the filename
+            array_shift($options);
 
-// @TODO Add proper handling of options
+            foreach ($options as $option => $optionValue) {
+                switch ($optionValue) {
+                    case is_numeric($optionValue) :
+                        if ($this->numberOfExercises) {
+                            echo 'cliRandy: number of exercises must be given only once.' . "\n";
+                            echo "Try `php cliRandy.php --help' for more information\n";
+                            exit();
+                        }
+                        $this->numberOfExercises = $optionValue;
+                        break;
+                    case '-n':
+                        $this->logging = false;
+                        break;
+                    case '--no-log':
+                        $this->logging = false;
+                        break;
+                    case '--help':
+                        exit($this->_usage());
+                        break;
+                    default:
+                        echo "cliRandy: invalid option -- '" . $optionValue . "'\n\n";
+                        echo "Try `php cliRandy.php --help' for more information\n";
+                        exit();
 
-$optionFound = false;
-$logging = true;
-$numberOfExercises = 0;
-
-array_shift($argv);
-foreach ($argv as $option => $optionValue) {
-    switch ($optionValue) {
-        case is_numeric($optionValue) :
-            if ($numberOfExercises) {
-                echo 'cliRandy: number of exercises must be given only once.' . "\n";
-                echo "Try `php cliRandy.php --help' for more information\n";
-                exit();
+                }
             }
-            $numberOfExercises = $optionValue;
-            break;
-        case '-n':
-            $logging = false;
-            break;
-        case '--no-log':
-            $logging = false;
-            break;
-        case '--help':
-            exit(usage());
-            break;
-        default:
-            echo "cliRandy: invalid option -- '" . $optionValue . "'\n\n";
-            echo "Try `php cliRandy.php --help' for more information\n";
-            exit();
+        }
 
+    }
+
+    private function _usage()
+    {
+        $usage = "Usage: php cliRandy.php [--no-log] [NUMBER OF EXERCISES]
+        Show <number> of exercises
+         php cliRandy.php <number>
+        
+        * start interactive mode
+         php cliRandy.php
+        
+        * In the interactive mode the following commands apply: 
+            q quits
+            enter key shows one exercise. 
+            Typing a number and then enter shows that number of exercises";
+        return $usage;
+    }
+
+    public function main($options)
+    {
+        $this->_checkOptions($options);
+
+        $exer = new CommandLineExerciser();
+        $exer->addView(new CommandLineView());
+
+        if ($this->logging) {
+            $exer->addView(new FileLoggingView());
+        }
+
+        if ($this->numberOfExercises > 0) {
+            $exer->showExercises($this->numberOfExercises);
+        } else {
+            $exer->startInteractiveMode();
+        }
     }
 }
 
-if ($logging) {
-    $exer->addView(new FileLoggingView());
-}
-
-if ($numberOfExercises > 0) {
-    $exer->showExercises($numberOfExercises);
-} else {
-    $exer->startInteractiveMode();
-}
+$driver = new ExerDriver();
+$driver->main($argv);
