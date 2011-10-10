b604a148df97f47491907278b32618debd7b5b7e
loota <heikki.naski@gmail.com>
2011-10-10 18:11:32 +0300

    Capitalize class name

--- RandyTest.php
+++ RandyTest.php
@@ -11,7 +11,7 @@ class OutputCapturingView implements View
     }
 }
  
-class randyTest extends PHPUnit_Framework_TestCase
+class RandyTest extends PHPUnit_Framework_TestCase
 {
 
     public function setUp()
